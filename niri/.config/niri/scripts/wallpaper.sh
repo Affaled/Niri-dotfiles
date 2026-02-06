0e6d69121754b06f4a18eb51b9af1e668c418675
¨Affaled¨ <affaledl@gmail.com>
2026-02-06 03:05:58 -0300

    correção de bug no caminho dos arquivos do matugen

--- niri/.config/niri/scripts/wallpaper.sh
+++ niri/.config/niri/scripts/wallpaper.sh
@@ -2,6 +2,7 @@
 DIR="/home/$USER/Wallpaper"
 WALLPAPER=$(find "$DIR" -type f \( -name "*.jpg" -o -name "*.png" \) | shuf -n 1)
 
+pkill swaybg
 swaybg -m fill -i "$WALLPAPER" &
 
-matugen image "$WALLPAPER" -t niri-colors.kdl -o "$HOME/.config/niri/colors.kdl"
+matugen image "$WALLPAPER"
